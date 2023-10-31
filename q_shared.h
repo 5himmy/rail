234e0d23c12cd7f73478f4df9b34c8f41159fb70
5himmy <572847+5himmy@users.noreply.github.com>
2023-11-01 00:59:30 +0300

    Update q_shared.h

--- q_shared.h
+++ q_shared.h
@@ -113,7 +113,7 @@ void Q_strlwr (char *str);
 int Q_vsnprintf (char *buff, size_t len, const char *fmt, va_list va);
 //int Q_snprintf (char *buff, size_t len, const char *fmt, ...);
 //#ifdef LINUX
-//#define	Q_DEBUGBREAKPOINT _Q_DEBUGBREAKPOINT()
+#define	Q_DEBUGBREAKPOINT _Q_DEBUGBREAKPOINT()
 //#define DEBUGBREAKPOINT __asm ("int $3")
 //#else
 //#define	Q_DEBUGBREAKPOINT ((void)0)
